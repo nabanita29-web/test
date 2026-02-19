ca8e8d506957a03e091967e19b85e7b060ca5daf
nabanita29-web <nabachowdhury2@gmail.com>
2026-02-19 21:43:58 +0530

    ch

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ app.use(express.json())
 app.post('/user', async (req, res) => {
     
     // Todo.query(`INSERT INTO users DEFAULT VALUES`)
-    const email = req.body.key1
-    const password = req.body.key2
-    Todo.query(`INSERT INTO users (email, password) VALUES ($1, $2)`,[email, password])
-    var result = await Todo.query(`SELECT * FROM users`)
+    const email = req.body.key1;
+    const password = req.body.key2;
+    Todo.query(`INSERT INTO users (email, password) VALUES ($1, $2)`,[email, password]);
+    var result = await Todo.query(`SELECT * FROM users`);
     // res.redirect(`/${rows}`)
     // res.redirect(`/?q='rows'`)
-    res.send(email)
+    res.send(email);
     // res.send(req.body)
     // res.send(userid)
 })
@@ -34,10 +34,10 @@ app.post('/user', async (req, res) => {
 
 
  app.get('/getdata', async (req, res) => {
-    const email = req.params.e
-    const todo = await Todo.query(`select * from todos where email=$1`,[email])
+    const email = req.query.e;
+    const todo = await Todo.query(`select * from users where email=$1`,[email]);
     // await Todo.query(`DELETE FROM todos`)
-    res.json(todo.rows)
+    res.json(todo);
 })
 app.post('/', (req, res) => {
     res.send("<h1>Hello!</h1>")
