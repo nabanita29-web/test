cf4fd91b1ac68752b9b4b0f8223caa4003e3f5b5
nabanita29-web <nabachowdhury2@gmail.com>
2026-02-19 20:44:21 +0530

    ch

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.post('/user', async (req, res) => {
 
 
  app.get('/getdata', async (req, res) => {
-    const email = req.body.e
+    const email = req.query.e
     const todo = await Todo.query(`select * from todos where email=$1`,[email])
     // await Todo.query(`DELETE FROM todos`)
     res.json(todo)
