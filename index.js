83cb87c994c9ddfa0e0a0e5028e166ee1253ce8f
nabanita29-web <nabachowdhury2@gmail.com>
2026-02-25 09:22:54 +0530

    ch

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/todoing', async (req, res) => {
     var result = await Todo.query(`SELECT * FROM users`);
     // res.redirect(`/${rows}`)
     // res.redirect(`/?q='rows'`)
-    res.send(email);
+    res.send(task);
     // res.send(req.body)
     // res.send(userid)
 })
